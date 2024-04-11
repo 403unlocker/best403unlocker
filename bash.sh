ffa7e49678b9ea72d6e71eb46e2db9c975ba7401
Arman Taheri <armantahery1381@gmail.com>
2024-04-11 03:46:01 +0330

    feat(): add variables by .env

--- bash.sh
+++ bash.sh
@@ -1,9 +1,3 @@
-#variables
-#file_url="https://packages.gitlab.com/gitlab/gitlab-ce/packages/el/7/gitlab-ce-16.8.0-ce.0.el7.x86_64.rpm/download.rpm"
-#dns="178.22.122.100 185.51.200.2 192.104.158.78 194.104.158.48 172.29.0.100 172.29.2.100 78.157.42.101 78.157.42.100 10.202.10.202 10.202.10.102 185.55.226.26 185.55.225.25 10.202.10.10 10.202.10.11 9.9.9.9 8.8.8.8 8.8.4.4 1.1.1.1 1.0.0.1"
-#timeout="10s"
-
-
 # Functions
 function change_dns () {
 
