8e3d976286a2b87ea841d824d7fe0bc05f7c37b3
Alireza <alish14.mod@gmail.com>
2024-04-25 01:52:45 +0330

    remov progress bar and add clear command

--- bash.sh
+++ bash.sh
@@ -1,26 +1,12 @@
 #!/bin/bash
 
-# Function to clear the screen and reset cursor position
-clear_screen() {
-    printf '\e[2J\e[H'
-}
 
-# Function to display the progress bar and step message
-progressbar() {
-    local width=50
-    local percent=$1
-    local complete=$((percent * width / 100))
-    local remaining=$((width - complete))
-    local step_message=$2  # Step message provided as argument
-
-    clear_screen
-    printf '\e[21;20H' #change cursor position
+display_step() {
+    local step_message=$1  # Step message provided as argument
+     clear 
+    reset
+    printf '\e[21;25H' 
     echo "Step: $step_message"
-    printf '\e[25;15H'
-    echo -ne "["
-    printf "%${complete}s" | tr ' ' '='
-    printf "%${remaining}s" | tr ' ' '.'
-    echo -ne "] $percent%\r"
 }
 
 change_dns() {
@@ -28,12 +14,9 @@ change_dns() {
     local step_message="Changing DNS to $dns_server"
 
     echo "nameserver $dns_server" > /etc/resolv.conf
-    cat /etc/resolv.conf
 
-    clear_screen
-
-    printf '\e[21;25H'
-    echo "DNS changed to $dns_server"
+    sleep 1
+    display_step "$step_message"
     echo " "
 }
 
@@ -41,14 +24,17 @@ download() {
     local timeout=$1
     local file_url=$2
     local filename=$3
-    local step_message="measuring $filename speed"
+    local step_message="Measuring $filename speed"
 
     timeout $timeout wget -q -O "/tmp/$filename" --no-dns-cache "$file_url"
-    for ((i = 0; i <= 100; i+=20)); do
 
-        progressbar $i "$step_message"
-        sleep 1
-    done
+    sleep 1
+    display_step "$step_message"
+
+    sleep 1
+
+    printf '\e[22;27H' 
+    echo "Downloaded $filename"
     echo  ""
 }
 
@@ -57,9 +43,11 @@ download_speed() {
 
     du -sh "/tmp/$filename" >> database
     rm "/tmp/$filename"
+
     sleep 1
 }
 
+# Main script
 
 touch database
 cp /etc/resolv.conf /etc/resolv.conf.backup
@@ -70,7 +58,7 @@ for dns_server in $dns; do
     download_speed "$dns_server"
 done
 
-clear_screen
+clear
 echo '*********************'
 echo "Best DNS server is $(sort -rn database | head -1 | cut -d'/' -f3)"
 echo '*********************'
