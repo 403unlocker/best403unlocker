d5ec78c689d8693caf16338d14723fc7918aa9e0
Kishmiish <65231756+Kishmiish@users.noreply.github.com>
2024-05-22 23:33:17 +0330

    Fixed sudo docker error (#22)
    * feat(): Add download option
    
    * feat(): handled sudo docker error
    
    * chore(): Implemented password_checker function to prompt for and verify sudo password before executing sudo commands
    
    ---------
    
    Co-authored-by: ArmanTaheriGhaleTaki <armantahery1381@gmail.com>

--- main.sh
+++ main.sh
@@ -3,6 +3,7 @@ source .env
 
 # Function to display the main menu
 main_menu() {
+    password=
     choice=$(whiptail --title "Best403Unlcoker TUI" --menu "Choose an option:" 15 60 4 \
     "Run DNS analyzer" "find the most speedful dns" \
     "Save file" "Downlaod file with the best dns" \
@@ -44,17 +45,34 @@ best_dns_finder() {
     selected_options=($(echo $choices | tr -d '"'))
     # Check if "docker" is in the selected options
     if [[ " ${selected_options[@]} " =~ " docker " ]]; then
-	docker run --env-file .env armantaherighaletaki/best403unlocker | tee log.txt 2>&1 
-    else 
-	wget -c https://raw.githubusercontent.com/ArmanTaheriGhaleTaki/best403unlocker/main/bash.sh && sudo bash bash.sh  | tee log.txt 2>&1
+	docker run --env-file .env armantaherighaletaki/best403unlocker 2>&1 | tee log.txt
+    status=$?
+
+    if [ $status -eq 0 ] && grep -q permission log.txt; then
+        password_checker
+        echo "$password" | sudo -S docker run --env-file .env armantaherighaletaki/best403unlocker | tee log.txt 2>&1
+    elif [ $status -eq 0 ]; then
+        whiptail --title "Error" --yesno "An error occurred. See log.txt for more info.\nDo you want to try again?" 15 60
+        status=$?
+        if [ $status -eq 0 ]; then
+            best_dns_finder
+        else
+            main
+        fi
+    fi
+    
+    else
+    password_checker
+	wget -c https://raw.githubusercontent.com/ArmanTaheriGhaleTaki/best403unlocker/main/bash.sh && echo $password | sudo -S bash bash.sh  | tee log.txt 2>&1
     fi
 	DNS=$(grep best log.txt| cut -d' ' -f5 )
 
     whiptail --title "DNS analyzer" --msgbox "Best DNS:\n$DNS" 15 60
 
     selected_options=($(echo $choices | tr -d '"'))
-    if whiptail --title "Confirmation" --yesno "set DNS to system'" 10 60 ;then
-    sudo bash -c "echo 'nameserver $DNS' > /run/systemd/resolve/stub-resolv.conf"
+    if whiptail --title "Confirmation" --yesno "set DNS to system" 10 60 ;then
+        password_checker
+        echo $password | sudo -S bash -c "echo 'nameserver $DNS' > /run/systemd/resolve/stub-resolv.conf"
     fi
 }
 
@@ -78,19 +96,47 @@ download_file_with_best_dns() {
     selected_options=($(echo $choices | tr -d '"'))
     # Check if "docker" is in the selected options
     if [[ " ${selected_options[@]} " =~ " docker " ]]; then
-	docker run --env-file .env armantaherighaletaki/best403unlocker | tee log.txt 2>&1 
-    else 
-	wget -c https://raw.githubusercontent.com/ArmanTaheriGhaleTaki/best403unlocker/main/bash.sh && sudo bash bash.sh  | tee log.txt 2>&1
+	docker run --env-file .env armantaherighaletaki/best403unlocker 2>&1 | tee log.txt
+    status=$?
+    
+    if [ $status -eq 0 ] && grep -q permission log.txt; then
+        password_checker
+        echo "$password" | sudo -S docker run --env-file .env armantaherighaletaki/best403unlocker | tee log.txt 2>&1
+    fi
+    
+    else
+    password_checker
+	wget -c https://raw.githubusercontent.com/ArmanTaheriGhaleTaki/best403unlocker/main/bash.sh && echo $password | sudo -S bash bash.sh  | tee log.txt 2>&1
     fi
 	DNS=$(grep best log.txt| cut -d' ' -f5 )
 
-    sudo bash -c "echo 'nameserver $DNS' > /run/systemd/resolve/stub-resolv.conf"
+    password_checker
+    echo "$password" | sudo -S bash -c "echo 'nameserver $DNS' > /run/systemd/resolve/stub-resolv.conf"
     wget --no-dns-cache $file_url -O $save_filepath 
-    sudo bash -c "cp /etc/resolv.conf.bakup /run/systemd/resolve/stub-resolv.conf"
+    echo "$password" | sudo -S bash -c "cp /etc/resolv.conf.bakup /run/systemd/resolve/stub-resolv.conf"
 }
 
 disconnect_network() {
 	echo hello 
+}
+
+# Checks the password if a sudo command is executed
+password_checker(){
+    password=
+    echo $password | sudo -S ls > /dev/null 2>&1
+    if [ $? -eq 1 ] && [[ -z $password ]]; then
+        while true; do
+            password=$(whiptail --title "Permission Denied" --passwordbox "Input your sudo password" 15 60 3>&1 1>&2 2>&3)
+            if [ $? -eq 0 ]; then
+                echo "$password" | sudo -S ls > /dev/null 2>&1
+                if [ $? -eq 0 ]; then
+                    break
+                fi
+            else
+                main
+            fi
+        done
+    fi
 }
 
 # Main function
